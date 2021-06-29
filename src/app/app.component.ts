438ffc54fb5f1f3b6849143aa23b8b847dcff5e0
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-06-29 07:39:18 -0700

    worked on the header selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipe-app';
+  show: string;
+
+  showRecipes(name: string) {
+    console.log(name);
+    this.show = name;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() selected = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onRecipesClicked() {
+    console.log('recipes');
+    this.selected.emit('recipes');
   }
 
+  onShoppingListClicked() {
+    this.selected.emit('shoppingList');
+  }
 }
